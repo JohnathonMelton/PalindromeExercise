cea4c619a452fdbf8898395f09dd8939f7804682
Johnathon Melton <seventh.acoustic@gmail.com>
2023-11-08 10:43:19 -0600

    Palindrome Exercise

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            if (word == null ||  word.Length == 0)
+            {
+                return false;
+            }
+            string lowerWord = word.ToLower();
+            string backwards = "";
+
+            for (int i = lowerWord.Length - 1; i >= 0; i--)
+            {
+                backwards += lowerWord[i];
+            }
+
+            if (lowerWord == backwards)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,9 +6,22 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hannah", true)]
+        [InlineData("Hannah", true)]
+        [InlineData("level", true)]
+        [InlineData("word", false)]
+        [InlineData("", false)]
+        [InlineData(null, false)]
+
+        public void IsAPalindrome(string word,  bool expected)
         {
+            var ws = new WordSmith();
+
+            var actual = ws.IsAPalindrome(word);
+
+            Assert.Equal(expected, actual);
 
         }
     }
